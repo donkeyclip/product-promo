41a4854b2377e2915dd581ecd5f92afda00d3438
ElisavetAmp <elisabet.ampatzidou@gmail.com>
2022-06-27 17:19:13 +0300

    fix: fix initParamsValidationRules

--- clip/initParamsValidationRules.js
+++ clip/initParamsValidationRules.js
@@ -1,101 +1,75 @@
 export default {
     color: {
       label: "Color",
-      type: "color",
-      optional: true,
-      default: "white",
+      type: "color"
     },
     textColor: {
         label: "Text Color",
         type: "color",
-        optional: true,
-        default: "#121212",
       },
     url: {
       label: "URL",
-      type: "string",
-      optional: true,
-      default: "docs.motorcortexjs.com",
+      type: "string"
     },
     brandName: {
       label: "Brand Name",
-      type: "string",
-      optional: true,
-      default: "Motorcortex",
+      type: "string"
     },
     firstSlideImageOne: {
       label: "First Slide Image One",
-      type: "string",
-      optional: true,
-      default: "https://donkeyclip.github.io/product-promo/demo/assets/first-slide1.jpg",
+      type: "string"
     },
     firstSlideImageTwo: {
       label: "First Slide Image Two",
-      type: "string",
-      optional: true,
-      default: "https://donkeyclip.github.io/product-promo/demo/assets/first-slide2.jpg",
+      type: "string"
     },
     transitionHeading: {
       label: "Transition Heading",
-      type: "string",
-      optional: true,
-      default: "Choose Convenience",
+      type: "string"
     },
     transitionSlogan: {
       label: "Transition Slogan",
-      type: "string",
-      optional: true,
-      default: "Shop with us",
+      type: "string"
     },
     bestProductsHeading: {
       label: "Best Products Heading",
-      type: "string",
-      optional: true,
-      default: "Wide assortment New Collection",
+      type: "string"
     },
     bestProductImages: {
       label: "Best Products Images",
       type: "array",
-      optional: true,
-      default: ["https://donkeyclip.github.io/product-promo/demo/assets/bp1.jpg","https://donkeyclip.github.io/product-promo/demo/assets/bp2.jpg","https://donkeyclip.github.io/product-promo/demo/assets/bp3.jpg"],
-      items:"string"
+      items:{
+        type:"string"
+      }
     },
     opHeading: {
       label: "First Slide Heading",
-      type: "string",
-      optional: true,
-      default: "Online Promotions",
+      type: "string"
     },
     opSlogan: {
       label: "First Slide Slogan",
-      type: "string",
-      optional: true,
-      default: "Best Prices",
+      type: "string"
     },
     opImages: {
       label: "First Slide Images",
       type: "array",
-      optional: true,
-      default: ["https://donkeyclip.github.io/product-promo/demo/assets/op1.jpg","https://donkeyclip.github.io/product-promo/demo/assets/op2.jpg","https://donkeyclip.github.io/product-promo/demo/assets/op3.jpg"],
-      items:"string"
+      items:{
+        type:"string"
+      }
     },
     fdHeading: {
       label: "Second Slide Heading",
-      type: "string",
-      optional: true,
-      default: "Fast delivery",
+      type: "string"
     },
     fdSlogan: {
       label: "Second Slide Slogan",
-      type: "string",
-      optional: true,
-      default: "Free from 50$ prurchese",
+      type: "string"
     },
     fdImages: {
       label: "Second Slide Images",
       type: "array",
-      optional: true,
-      default: ["https://donkeyclip.github.io/product-promo/demo/assets/fd1.jpg","https://donkeyclip.github.io/product-promo/demo/assets/fd2.jpg","https://donkeyclip.github.io/product-promo/demo/assets/fd3.jpg","https://donkeyclip.github.io/product-promo/demo/assets/fd4.jpg","https://donkeyclip.github.io/product-promo/demo/assets/fd5.jpg"],
-      items:"string"
+      items:{
+        type:"string"
+      }
     },
 };
